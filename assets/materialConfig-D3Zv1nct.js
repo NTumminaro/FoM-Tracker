53eec1df57884a800bec52eab7b3b8b70422d689
Nicholas Tumminaro <nicholas.tumminaro@DNWD4KH40L.local>
2024-08-29 16:37:17 -0500

    Updates

---
+++ assets/materialConfig-D3Zv1nct.js
@@ -0,0 +1,1 @@
+const t={name:"Material",defaultSize:{width:"22%",height:"30%"},filterGroups:{locations:{title:"Locations",filters:[{type:"mine",label:"Mine"}]},attributes:{title:"Attributes",filters:[{type:"museum",label:"Museum"}]}},sortOptions:[],tooltipFields:["source","museumAndDiveable"]};export{t as default};
